857133f09934018b0ee799f8f85a5dbdda7f6bca
Pietro Di Bello <pierodibello@gmail.com>
2025-07-06 00:12:14 +0200

    test: improve the integration test

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,11 @@ fn main() {
     let args = env::args().collect::<Vec<_>>();
     let config = Config::new(&args);
 
-    println!("[LOG] Searching for {}", config.query);
-    println!("[LOG] In file {}", config.file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let content = fs::read_to_string(config.file_path.clone())
+    let _content = fs::read_to_string(config.file_path.clone())
         .unwrap_or_else(|e| panic!("Could not read file {}: {}", config.file_path, e));
-
-    println!("{content}");
 }
 
 struct Config {
--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -1,26 +1,42 @@
-use std::process::Command;
+use std::fs;
+use std::process::{Command, Output};
 
 #[test]
 fn runs_with_search_string_and_filename() {
-    std::fs::write("test_file.txt", poem()).expect("Failed to write test file");
+    fs::write("test_file.txt", poem()).expect("Failed to write test file");
 
     let output = Command::new(env!("CARGO_BIN_EXE_minigrep"))
-        .arg("searchstring")
+        .arg("a_search_string")
         .arg("test_file.txt")
         .output()
         .expect("failed to execute binary");
 
+    assert_command_succeeded(&output);
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    assert_contains(&stdout, "Searching for a_search_string");
+    assert_contains(&stdout, "In file test_file.txt");
+
+    let stderr = String::from_utf8_lossy(&output.stderr);
+    assert_is_empty(&stderr);
+
+    fs::remove_file("test_file.txt").ok();
+}
+
+fn assert_is_empty(string: &str) {
+    assert!(string.is_empty(), "Expected empty, but got: {string}");
+}
+
+fn assert_contains(string: &str, expected: &str) {
+    assert!(string.contains(expected), "Actual string: {string}");
+}
+
+fn assert_command_succeeded(output: &Output) {
     assert!(
         output.status.success(),
         "Expected command to succeed, but got stderr: {}",
         String::from_utf8_lossy(&output.stderr)
     );
-
-    let stdout = String::from_utf8_lossy(&output.stdout);
-    assert!(stdout.contains("Hello"), "Actual stdout: {}", stdout);
-
-    // Clean up the test file. If the cleanup fails, ignore it for the test.
-    std::fs::remove_file("test_file.txt").ok();
 }
 
 fn poem() -> &'static str {
