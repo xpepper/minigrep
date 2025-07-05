c005dd3c6b2ce33df6ffcf6f6d87bd5deeeb22a6
Pietro Di Bello <pierodibello@gmail.com>
2025-07-05 17:54:08 +0200

    refactor: extract parsing logic

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,16 @@ use std::{env, fs};
 
 fn main() {
     let arguments = env::args().collect::<Vec<_>>();
-    let _query = &arguments[1];
-    let file_path = &arguments[2];
+    let (_query, file_path) = parse_config(&arguments);
 
     let content = fs::read_to_string(file_path)
         .unwrap_or_else(|e| panic!("Could not read file {}: {}", file_path, e));
 
     println!("{content}");
+}
+
+fn parse_config(arguments: &Vec<String>) -> (&String, &String) {
+    let query = &arguments[1];
+    let file_path = &arguments[2];
+    (query, file_path)
 }
