fcebad22572f46f35382be46a9aa55e7171cb303
Pietro Di Bello <pierodibello@gmail.com>
2025-07-05 18:08:46 +0200

    refactor: extract a Config stuct to hold the program arguments

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 use std::{env, fs};
 
 fn main() {
-    let arguments = env::args().collect::<Vec<_>>();
-    let (_query, file_path) = parse_config(&arguments);
+    let args = env::args().collect::<Vec<_>>();
+    let config = parse_config(&args);
 
-    let content = fs::read_to_string(file_path)
-        .unwrap_or_else(|e| panic!("Could not read file {}: {}", file_path, e));
+    println!("[LOG] Searching for {}", config.query);
+    println!("[LOG] In file {}", config.file_path);
+
+    let content = fs::read_to_string(config.file_path.clone())
+        .unwrap_or_else(|e| panic!("Could not read file {}: {}", config.file_path, e));
 
     println!("{content}");
 }
 
-fn parse_config(arguments: &[String]) -> (&str, &str) {
-    let query = &arguments[1];
-    let file_path = &arguments[2];
-    (query, file_path)
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(arguments: &[String]) -> Config {
+    let query = arguments[1].clone();
+    let file_path = arguments[2].clone();
+    Config { query: query, file_path }
 }
