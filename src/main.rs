1b2ea306d97fbf13b4185b75b6a5093e12b6cb39
Pietro Di Bello <pierodibello@gmail.com>
2025-07-13 00:02:40 +0200

    feat: printing errors to stderr instead of stdin

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     let args = env::args().collect::<Vec<_>>();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Cannot parse arguments: {err}");
+        eprintln!("Cannot parse arguments: {err}");
         process::exit(1);
     });
 
     minigrep::run(&config).unwrap_or_else(|e| {
-        println!("Could not read file {}: {e}", config.file_path);
+        eprintln!("Could not read file {}: {e}", config.file_path);
         process::exit(1)
     });
 }
