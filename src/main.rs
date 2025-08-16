dcad3e5ca8d683d22750d4d954ace8305ba52edd
Pietro Di Bello <pierodibello@gmail.com>
2025-08-16 19:40:30 +0200

    refactor: update package name to minigrep-tool in main.rs, Cargo.lock, and Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use minigrep::Config;
+use minigrep_tool::Config;
 use std::{env, process};
 
 fn main() {
@@ -7,7 +7,7 @@ fn main() {
         process::exit(1);
     });
 
-    minigrep::run(&config).unwrap_or_else(|e| {
+    minigrep_tool::run(&config).unwrap_or_else(|e| {
         eprintln!("Could not read file {}: {e}", config.file_path);
         process::exit(1)
     });
